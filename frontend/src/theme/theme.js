b2b05de6a22dfbd6c38656f9b1d2751d573ddf4f
Colin de Vries <colindevries325@gmail.com>
2020-01-06 17:46:24 -0800

    remove unnecessary boilerplate code. add theme

---
+++ frontend/src/theme/theme.js
@@ -0,0 +1,39 @@
+export default {
+  breakpoints: ['40em', '52em', '64em'],
+  fontSizes: [
+    '1.2rem',
+    '1.4rem',
+    '1.6rem',
+    '2.0rem',
+    '2.4rem',
+    '3.2rem',
+    '4.8rem',
+    '6.4rem'
+  ],
+  colors: {
+    blue: '#07c',
+    lightgray: '#f6f6ff'
+  },
+  space: [
+    0,
+    '0.4rem',
+    '0.8rem',
+    '1.6rem',
+    '3.2rem',
+    '6.4rem',
+    '12.8rem',
+    '25.6rem'
+  ],
+  fonts: {
+    body: 'system-ui, sans-serif'
+  },
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.25
+  }
+};
